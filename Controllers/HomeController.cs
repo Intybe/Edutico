9a44303e15c13c8261733b2ba61330a32b723ddb
FlaviaAMorgado <flavia.morgado@etec.sp.gov.br>
2024-09-01 22:38:19 -0300

    tela de layout pronta falta ajustar a apre do conteudo. tela produtos do funcionário 80% faltando ajustar

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using edutico.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace edutico.Controllers
+{
+    public class HomeController : Controller
+
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ProdutosF()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
